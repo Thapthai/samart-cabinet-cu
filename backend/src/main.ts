9b54e355572d992e43b0da03bdc6f9eafeedf898
Thapthai <knight.thapthai@gmail.com>
2026-08-17 14:13:20 +0700

    Update environment configuration and dependencies for improved functionality
    - Modified `.env.example` to include new environment variables and updated database connection details for better clarity and usability.
    - Adjusted `main.ts` to dynamically set the API prefix using the new `API_NAME` environment variable, enhancing flexibility in API routing.
    - Updated `package-lock.json` to reflect dependency upgrades, ensuring compatibility and security with the latest package versions.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/smart-cabinet-cu/v1');
+  app.setGlobalPrefix('api/' + process.env.API_NAME + '/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -18,14 +18,16 @@ async function bootstrap() {
   const allowedOrigins = process.env.CORS_ORIGIN
     ? process.env.CORS_ORIGIN.split(',').map((o) => o.trim())
     : [
-        'http://localhost:3100',
-        'http://127.0.0.1:3100',
-        'http://localhost:4100',
-        'http://127.0.0.1:4100',
-        'http://10.11.9.84:3100',
-        'http://10.11.9.84:4100',  
-        'https://phc.dyndns.biz/smart-cabinet-cu',
-      ];
+      'http://localhost:3100',
+      'http://127.0.0.1:3100',
+      'http://localhost:4100',
+      'http://127.0.0.1:4100',
+      'http://10.11.9.84:3100',
+      'http://10.11.9.84:4100',
+      'https://phc.dyndns.biz',
+      'https://poseintelligence.co.th',
+      'https://www.poseintelligence.co.th',
+    ];
   app.enableCors({
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
